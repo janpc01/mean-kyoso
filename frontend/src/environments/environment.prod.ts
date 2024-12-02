ed20663675d7f9e28221d113a383b6f2304cde6b
Jan <janpc01@gmail.com>
2024-12-02 12:06:26 -0700

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://your-production-api-url.com/api',
-    stripePublishableKey: 'your_live_publishable_key'
+    apiUrl: 'https://kyoso-backend-dhheg8akajdre6ce.canadacentral-01.azurewebsites.net/api',
+    stripePublishableKey: 'pk_test_51QMlBeKDLvEmLtClMuR2LhouEhOyclSDcEx2K5yLsOsnb3lECXj7dvYoHrF506DXBxEOtmIscmiAMxqnevwXLvGv00y79Lv8ze'
   };
