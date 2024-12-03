31db74edb05b6a81829fc694dd2b315fe8d5a84d
Jan <janpc01@gmail.com>
2024-12-03 11:10:30 -0700

    Update auth.routes.js

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -2,16 +2,6 @@ const controller = require("../controllers/auth.controller");
 const verifySignUp = require("../middlewares/verifySignUp");
 
 module.exports = function(app) {
-  app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://kyosocards.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
   app.post("/api/auth/signup", [verifySignUp.checkDuplicateEmail], controller.signup);
   app.post("/api/auth/signin", controller.signin);
   app.post("/api/auth/signout", controller.signout);
