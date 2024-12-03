f3539d4e4ee9556f15324163a660e66254a4b133
Jan <janpc01@gmail.com>
2024-12-03 14:48:22 -0700

    Update server-prod.js

--- backend/server-prod.js
+++ backend/server-prod.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieSession = require('cookie-session');
@@ -91,7 +90,7 @@ require('./app/routes/payment.routes')(app);
 require('./app/routes/email.routes')(app);
 
 // Set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
